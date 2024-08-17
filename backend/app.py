31835aceace857fa67ed7346ffd34170a49b8993
KARTHI2710 <bskarthikeyan2710@gmail.com>
2024-08-17 20:01:39 +0530

    basic templete created for flask

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,18 @@
-print('hello world')
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///friends.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+import routes
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()  # Create database tables
+    app.run(debug=True)
