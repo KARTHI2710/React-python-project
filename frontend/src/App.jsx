5284a1407e708ce98e6798aa91e992987ffb6fcf
KARTHI2710 <bskarthikeyan2710@gmail.com>
2024-09-10 19:59:42 +0530

    Feat : Modal component - pending

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,31 +5,40 @@ import Model from "./components/model/Model.jsx"
 
 function App() {
   const [users,setUsers]=useState([])
+  const [ismodelopen,setIsmodelopen]=useState(false)
 
 
-useEffect(()=>{
-    const getUsers = async () =>{
-      try{
-        const res = await fetch('http://127.0.0.1:5000/api/friends')
-        const data =await res.json();
-        console.log(data);
-        if(!res.ok){
-          throw new Error(data.error);
+  useEffect(()=>{
+      const getUsers = async () =>{
+        try{
+          const res = await fetch('http://127.0.0.1:5000/api/friends')
+          const data =await res.json();
+          console.log(data);
+          if(!res.ok){
+            throw new Error(data.error);
+          }
+          setUsers(data)
+        }catch(error){
+          console.error(error);
+        }finally{
+
         }
-        setUsers(data)
-      }catch(error){
-        console.error(error);
-      }finally{
+      } 
+      getUsers();
+  },[]
+  )
+  
+  //open the model
+  function addfriend(){
+    setIsmodelopen(true);
+    // setIsmodelopen(prevState => !prevState);
+  }
 
-      }
-    } 
-    getUsers();
-},[]
-)
   return (
     <>
         <div className="title">
           My Friends
+          <button onClick={addfriend}>Add Friend</button>
         </div>
         <div className="cards">
           {users.map((user) => (
@@ -39,7 +48,7 @@ useEffect(()=>{
         </div>
 
         
-        {/* <Model /> */}
+        {ismodelopen && <Model /> }
 
     </>
   )
--- frontend/src/components/model/Model.jsx
+++ frontend/src/components/model/Model.jsx
@@ -3,7 +3,58 @@ import "./Model.css"
 const Model = () => {
   return (
     <div className="modalcontainer">
-      <h1>model</h1>
+      <div className="modal">
+          <div className="heading">
+            <h3>Add Friend</h3>
+            <span>&times;</span>
+          </div>
+          <div className="name-age">
+            <div className="form-control">
+              <label htmlFor="name" className="form-label">
+                Full Name
+                <input type="text" id="name" className="form-text" />
+              </label>
+            </div>
+            <div className="form-control">
+              <label htmlFor="name" className="form-label">
+                Role
+                <input type="text" id="name" className="form-text" />
+              </label>
+            </div>
+          </div>
+
+          <div className="description">
+            <label htmlFor="desc">
+              Description
+              <textarea name="descrition" id="desc" className="form-desc"></textarea>
+            </label>
+          </div>
+          <div className="gender">
+            <label>
+              <input
+                type="radio"
+                value="male"
+              />
+              Male
+            </label>
+  
+            <label>
+              <input
+                type="radio"
+                value="female"
+              />
+              Female
+            </label>
+
+          </div>
+
+          <div className="buttons">
+            <button className="add">Add</button>
+            <button className="cancel">cancel</button>
+          </div>
+
+      </div>
+      
       
     </div>
   )
